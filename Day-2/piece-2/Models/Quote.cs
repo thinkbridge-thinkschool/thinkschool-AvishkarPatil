34c79fbbe232a07e108105e2bcee71f6e15d0d02
Avishkar096 <avishkarpatil071@gmail.com>
2026-05-20 12:02:44 +0530

    Convert piece-2 from submodule to normal folder

---
+++ Day-2/piece-2/Models/Quote.cs
@@ -0,0 +1,12 @@
+namespace QuotesApi.Models;
+
+public class Quote
+{
+    public int Id { get; set; }
+
+    public string Author { get; set; } = string.Empty;
+
+    public string Text { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
